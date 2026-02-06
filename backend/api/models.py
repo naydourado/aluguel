91d5ea78bb04bb6acab677b1e99dc26756c3561a
naydourado <naydouradooliveira@gmail.com>
2026-02-06 11:23:42 -0300

    alterações

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,7 +22,7 @@ class Usuario(models.Model):
         return self.nome
     
 class Imovel(models.Model):
-    tipo = models.CharField(max_length=10)
+    tipo = models.CharField(max_length=10) # AQUI ERA TITULO, NÃO TIPO!
     tipo = models.CharField(max_length=1-00)
     valor_aluguel =  models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20)
